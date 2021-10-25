6fbaa42a7ec2a868dd0e9b0b881e9519c428c9e6
RAJ MANJREKAR <manjrekarraj9@gmail.com>
2021-10-25 14:30:06 +0530

    added reverse linked list java program

---
+++ reverseLinkedList.java
@@ -0,0 +1,56 @@
+// Java program for reversing the linked list
+
+class LinkedList {
+
+	static Node head;
+
+	static class Node {
+
+		int data;
+		Node next;
+
+		Node(int d) {
+			data = d;
+			next = null;
+		}
+	}
+
+	/* Function to reverse the linked list */
+	Node reverse(Node node) {
+		Node prev = null;
+		Node current = node;
+		Node next = null;
+		while (current != null) {
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;
+		}
+		node = prev;
+		return node;
+	}
+
+	void printList(Node node) {
+		while (node != null) {
+			System.out.print(node.data + " ");
+			node = node.next;
+		}
+	}
+
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		list.head = new Node(1);
+		list.head.next = new Node(2);
+		list.head.next.next = new Node(3);
+		list.head.next.next.next = new Node(4);
+		
+		System.out.println("Given Linked list");
+		list.printList(head);
+		head = list.reverse(head);
+		System.out.println("");
+		System.out.println("Reversed linked list ");
+		list.printList(head);
+	}
+}
+
+
