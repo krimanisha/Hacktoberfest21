cc6906dabc33c9b2e89facc5f3f47b32b0784bb4
dewangga12 <dewanggajr30@gmail.com>
2021-11-02 13:32:23 +0700

    add java project

---
+++ project/BangunDatar.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritance_amelia;
+
+/**
+ *
+ * @author Amelia
+ */
+public class BangunDatar {
+    public float luas(){
+        System.out.print("Luas bangun datar     : ");
+        return 0;
+    }
+    
+    public float keliling(){
+        System.out.print("Keliling bangan datar : ");
+        return 0;
+    }
+}
