fd220fd3853e7fd3591538657ceba655fd559acb
Govind Kumar <35293008+govind144@users.noreply.github.com>
2022-10-07 23:59:03 +0530

    PriorityQueueDemo

---
+++ PriorityQueueDemo.java
@@ -0,0 +1,26 @@
+import java.util.*; 
+  
+class PriorityQueueDemo { 
+    
+      // Main Method 
+    public static void main(String args[]) 
+    { 
+        // Creating empty priority queue 
+        PriorityQueue<Integer> pQueue = new PriorityQueue<Integer>(); 
+  
+        // Adding items to the pQueue using add() 
+        pQueue.add(10); 
+        pQueue.add(20); 
+        pQueue.add(15); 
+  
+        // Printing the top element of PriorityQueue 
+        System.out.println(pQueue.peek()); 
+  
+        // Printing the top element and removing it 
+        // from the PriorityQueue container 
+        System.out.println(pQueue.poll()); 
+  
+        // Printing the top element again 
+        System.out.println(pQueue.peek()); 
+    } 
+} 
